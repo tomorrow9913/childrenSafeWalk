a2a02c55696a175ec17b1fce3e46d39c1febbeb8
tomorrow9913 <tomorrow9913@gmail.com>
2020-07-29 19:57:36 +0900

    feat : 로그인 시 맵 화면으로 전송
    데이터 전송이 필요함
    
    See also: #3

---
+++ android/app/src/main/java/kr/co/woobi/tomorrow99/safewalk/mainmapPage.kt
@@ -0,0 +1,12 @@
+package kr.co.woobi.tomorrow99.safewalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mainmapPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainmap_page)
+    }
+}
