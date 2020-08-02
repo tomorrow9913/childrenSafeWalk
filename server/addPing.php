4de403f2ee9da0cf18acfb3d520611538de5216c
Piorosen <aoikazto@gmail.com>
2020-08-03 00:04:43 +0900

    feat : 핑정보를 추가하는 API 구현

---
+++ server/addPing.php
@@ -0,0 +1,47 @@
+<?php
+include('./library/connect.php');
+
+$conn = connect();
+
+$rawPOST = file_get_contents("php://input");
+$decode = json_decode($rawPOST, true);
+
+$result = array();
+if ($decode == null){
+    $json["result"] = "fail";
+    $json["comment"] = "json error";
+    echo(json_encode($json));
+    disconnect($conn);
+    return;
+}
+
+$session = $decode['session'];
+$location = $decode['location'];
+$location = makePoint($location); 
+
+$level = $decode['level'];
+
+$tag = $decode['tag'];
+$tag = implode(',', $tag);
+
+if (checkSession($conn, $session)) {
+    $sql = "INSERT INTO pingInfo(location, level, tag) values(PointFromText('$location'), '$level', '$tag')";
+    if (!mysqli_query($conn, $sql)) {
+        $result["result"] = "fail";
+        $result["comment"] = mysqli_error($conn);
+    }else {
+        $sql = "SELECT id from pingInfo ORDER BY id DESC";
+        $query = mysqli_query($conn, $sql);
+        $row = (int)mysqli_fetch_array($query)[0];
+        $result["result"] = "success";
+        $result["id"] = $row;
+    }
+}else {
+    $result["result"] = "fail";
+    $result["comment"] = "session is expired";
+}
+
+echo(json_encode($result));
+
+disconnect($conn);
+?>
