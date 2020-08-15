0944ca07b625afedc6aa2144310b3dda07140270
tomorrow9913 <tomorrow9913@gmail.com>
2020-08-15 17:34:22 +0900

    chore : 폴더구조 변경
    폴더 구조 변경 및 glide 활용을 위한 gladle 설정 변경

---
+++ android/app/src/main/java/kr/co/woobi/tomorrow99/safewalk/sign/signup.kt
@@ -0,0 +1,21 @@
+package kr.co.woobi.tomorrow99.safewalk.sign
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+data class SignupOut (
+    var result:String,      //"result": "success" or "fail"
+    var comment:String?     //"reason for error"
+)
+
+interface SignUpService {
+    //@FormUrlEncoded
+    @POST(value = "registerUser.php")
+    @Headers("Content-Type: application/json")
+
+    fun requestSignUP (
+        @Body params: HashMap<String, String?>
+    ) : Call<SignupOut>
+}
