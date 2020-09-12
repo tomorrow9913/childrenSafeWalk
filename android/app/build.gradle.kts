c5c675a53b1c3e4b7cfbeb732deb8ccd8ba14008
sungbin5304 <sungbin.dev@gmail.com>
2020-09-13 00:48:19 +0900

    apply gradle-dsl

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,97 @@
+plugins {
+    id("com.android.application")
+    id("dagger.hilt.android.plugin")
+    id("name.remal.check-dependency-updates") version "1.0.211"
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Application.compileSdk)
+    defaultConfig {
+        minSdkVersion(Application.minSdk)
+        targetSdkVersion(Application.targetSdk)
+        versionCode = Application.versionCode
+        versionName = Application.versionName
+        multiDexEnabled = true
+        setProperty("archivesBaseName", "v$versionName ($versionCode)")
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+    }
+
+    packagingOptions {
+        exclude("META-INF/DEPENDENCIES")
+        exclude("META-INF/library_release.kotlin_module")
+    }
+
+    compileOptions {
+        sourceCompatibility = Application.sourceCompat
+        targetCompatibility = Application.targetCompat
+    }
+
+    kotlinOptions {
+        jvmTarget = Application.jvmTarget
+    }
+}
+
+dependencies {
+    fun def(vararg strings: String) {
+        for (string in strings) implementation(string)
+    }
+
+    def(
+        Dependencies.Map.Location,
+        Dependencies.Map.Map,
+
+        Dependencies.Network.Retrofit,
+        Dependencies.Network.OkHttp,
+
+        Dependencies.Rx.Kotlin,
+        Dependencies.Rx.Android,
+        Dependencies.Rx.Retrofit,
+
+        Dependencies.Essential.AppCompat,
+        Dependencies.Essential.Anko,
+        Dependencies.Essential.Kotlin,
+
+        Dependencies.Ktx.Core,
+        Dependencies.Ktx.Fragment,
+
+        Dependencies.Di.Hilt,
+
+        Dependencies.Ui.ShapeOfYou,
+        Dependencies.Ui.YoYo,
+        Dependencies.Ui.Lottie,
+        Dependencies.Ui.Licenser,
+        Dependencies.Ui.Material,
+        Dependencies.Ui.Glide,
+        Dependencies.Ui.ConstraintLayout,
+
+        Dependencies.Utils.GsonConverter,
+        Dependencies.Utils.YoyoHelper,
+        Dependencies.Utils.AndroidUtils,
+        Dependencies.Utils.CrashReporter
+    )
+
+    kapt(Dependencies.Di.HiltCompiler)
+    kapt(Dependencies.Utils.GlideCompiler)
+}
---
+++ android/build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:${Versions.Essential.Gradle}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.Essential.Kotlin}")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.Di.Hilt}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven { url = uri("https://navercorp.bintray.com/maps") }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.google.com") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
