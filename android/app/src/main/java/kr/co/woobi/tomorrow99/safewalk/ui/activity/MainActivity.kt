7f2381792ea81aee7ee14b78de232b45a3be9032
sungbin5304 <sungbin.dev@gmail.com>
2020-09-17 10:25:06 +0900

    :unicorn:

--- android/app/src/main/java/kr/co/woobi/tomorrow99/safewalk/ui/activity/MainActivity.kt
+++ android/app/src/main/java/kr/co/woobi/tomorrow99/safewalk/ui/activity/MainActivity.kt
@@ -3,10 +3,13 @@ package kr.co.woobi.tomorrow99.safewalk.ui.activity
 import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.sungbin.sungbintool.util.ToastUtil
+import kotlinx.android.synthetic.main.activity_main.*
 import kr.co.woobi.tomorrow99.safewalk.R
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -18,11 +21,31 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         (supportFragmentManager.findFragmentById(R.id.fcv_map) as? MapFragment?)?.getMapAsync(this)
 
         TedPermission.with(this)
-            .setPermissionListener(permissionlistener)
-            .setDeniedMessage()
-            .setPermissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+            .setPermissionListener(object : PermissionListener {
+                override fun onPermissionGranted() {
+                    fab_location.performClick()
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    ToastUtil.show(
+                        applicationContext,
+                        getString(R.string.main_permission_denied),
+                        ToastUtil.SHORT,
+                        ToastUtil.INFO
+                    )
+                }
+
+            })
+            .setDeniedMessage(R.string.main_permission_denied)
+            .setPermissions(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
             .check()
 
+        fab_location.setOnClickListener {
+            btn_location.performClick()
+        }
     }
 
     override fun onMapReady(map: NaverMap) {
