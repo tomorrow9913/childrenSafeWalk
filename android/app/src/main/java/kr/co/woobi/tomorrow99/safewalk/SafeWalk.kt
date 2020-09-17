b15d93ca013139d1bb047a0f21d772c1ecc38f21
sungbin5304 <sungbin.dev@gmail.com>
2020-09-17 18:46:02 +0900

    이거 왜 커밋이 안됬지

--- android/app/src/main/java/kr/co/woobi/tomorrow99/safewalk/SafeWalk.kt
+++ android/app/src/main/java/kr/co/woobi/tomorrow99/safewalk/SafeWalk.kt
@@ -11,4 +11,13 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class SafeWalk : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+            // todo: 예상치 못한 오류 처리 -> 릴리즈 할 때 밑 코드 주석 지우기
+            // ExceptionUtil.except(Exception(throwable), applicationContext)
+        }
+    }
+
 }
